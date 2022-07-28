07bd6b5c419882331f2c6ebb6492b84c272c08aa
Dionilton <dionilton.os@gmail.com>
2022-07-28 16:16:51 -0300

    sistema de login com validação em banco de dados AWS

--- APP_TdG/kivy_study/main.py
+++ APP_TdG/kivy_study/main.py
@@ -1,5 +1,19 @@
+import dotenv
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
+import psycopg2 as pg
+from sqlalchemy import create_engine
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+str_conn = os.getenv("str_conn")
+engine = create_engine(str_conn)
+sql = "SELECT user_id, usuario, senha, nome from tbusers"
+
+df = pd.read_sql_query(sql, con=engine)
 
 
 class JanelaManage(ScreenManager):
@@ -25,16 +39,16 @@ class MeuApp(MDApp):
         self.title = "Login System Test"
 
     def isAuthenticated(self):
-        if 5 > 15:
-            self.root.current = 'principal'
+        user = self.root.get_screen('login').ids.user.text
+        password = self.root.get_screen('login').ids.password.text
+        if user in df['usuario'].values:
+            data = df.loc[df.usuario == user]
+            if password == data['senha'].values[0]:
+                self.root.current = 'principal'
+            else:
+                self.root.get_screen('login').ids.msg.text = 'Usuário ou senha incorreto. Tente novamente!'
         else:
             self.root.get_screen('login').ids.msg.text = 'Usuário ou senha incorreto. Tente novamente!'
 
-        """
-        user = self.root.get_screen('login').ids.user.text
-        password = self.root.get_screen('login').ids.password.text
-        print(user)
-        print(password)
-        """
 
 MeuApp().run()
