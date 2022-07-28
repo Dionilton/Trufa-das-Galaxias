f5d063d243c0ac7528a44126e1befc6b18fb3a9b
Dionilton <dionilton.os@gmail.com>
2022-07-28 02:18:45 -0300

    cmt

--- APP_TdG/kivy_study/main.py
+++ APP_TdG/kivy_study/main.py
@@ -25,10 +25,16 @@ class MeuApp(MDApp):
         self.title = "Login System Test"
 
     def isAuthenticated(self):
+        if 5 > 15:
+            self.root.current = 'principal'
+        else:
+            self.root.get_screen('login').ids.msg.text = 'Usuário ou senha incorreto. Tente novamente!'
+
+        """
         user = self.root.get_screen('login').ids.user.text
         password = self.root.get_screen('login').ids.password.text
         print(user)
         print(password)
-
+        """
 
 MeuApp().run()
